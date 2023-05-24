58b1e67d9254a46f3b8f392517652f71399df332
Sviatoslav <lughin.sviatoslav@mail.ru>
2023-05-24 10:00:00 +0300

    PRA-11 setup eslint and prettier

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,19 +1,21 @@
-import { useEffect } from 'react'
-import './App.css'
-import styles from './styles.module.scss'
+import { useEffect } from 'react';
+import './App.css';
+import styles from './styles.module.scss';
 
 function App() {
   useEffect(() => {
     const fetchServerData = async () => {
-      const url = `http://localhost:${__SERVER_PORT__}`
-      const response = await fetch(url)
-      const data = await response.json()
-      console.log(data)
-    }
+      const url = `http://localhost:${__SERVER_PORT__}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+    };
 
-    fetchServerData()
-  }, [])
-  return <div className={styles.app}>Вот тут будет жить ваше приложение :)</div>
+    fetchServerData();
+  }, []);
+  return (
+    <div className={styles.app}>Вот тут будет жить ваше приложение :)</div>
+  );
 }
 
-export default App
+export default App;
