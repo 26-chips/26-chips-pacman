1cebcdb97909526fa587de8d2531f85b97d0aac9
gitnbrsp <no@e-mail.com>
2023-05-28 00:36:42 +0300

    Edit Input on UI page

--- packages/client/src/pages/UIPage/UIPage.tsx
+++ packages/client/src/pages/UIPage/UIPage.tsx
@@ -1,5 +1,4 @@
 import { Input } from '../../components';
-import { validateFn } from './additionalFunctions';
 import { FunctionComponent } from 'react';
 
 const UIPage = () => {
@@ -7,20 +6,25 @@ const UIPage = () => {
     <>
       <h1>UI Page</h1>
 
-      <Input
-        value=""
-        label="Input 1"
-        isLabelStatic={false}
-        deleteSymbol=" × "
-        validateFn={validateFn}
-        errorMessage="No numbers allowed"></Input>
+      <h3>Error input 1</h3>
 
       <Input
-        value=""
-        label="Input 2"
-        isLabelStatic={true}
-        validateFn={validateFn}
-        errorMessage="No numbers allowed"></Input>
+        title="Input 1"
+        isLabelStatic={false}
+        deleteSymbol=" × "
+        errorMessage="Error"></Input>
+
+      <h3>Valid input 1</h3>
+
+      <Input title="Input 1" isLabelStatic={false} deleteSymbol=" × "></Input>
+
+      <h3>Valid input 2</h3>
+
+      <Input title="Input 2" isLabelStatic={true}></Input>
+
+      <h3>Error input 2</h3>
+
+      <Input title="Input 2" isLabelStatic={true} errorMessage="Error"></Input>
     </>
   );
 };
