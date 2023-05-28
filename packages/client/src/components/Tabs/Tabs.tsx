c2a73e85ad5251870a4e7880fc549ba501dedf57
gitnbrsp <no@e-mail.com>
2023-05-28 23:42:07 +0300

    init

---
+++ packages/client/src/components/Tabs/Tabs.tsx
@@ -0,0 +1,33 @@
+import { LiHTMLAttributes, MouseEvent, useState } from 'react';
+import cn from 'classnames';
+import styles from './tabs.module.scss';
+
+interface TabsProps extends LiHTMLAttributes<HTMLLIElement> {
+  tabNames: string[];
+  activeTab?: string;
+}
+
+export const Tabs = (props: TabsProps) => {
+  const { tabNames, activeTab, ...otherProps } = props;
+  const [currentTab, setCurrentTab] = useState(activeTab);
+
+  //Заглушка для прощелкивания табов
+  const toggleTab = (e: MouseEvent<HTMLUListElement>) => {
+    setCurrentTab((e.target as HTMLLIElement).innerText.toString());
+  };
+
+  return (
+    <div className={styles.container}>
+      <ul onClick={toggleTab} className={styles.ul}>
+        {tabNames.map(tab => (
+          <li
+            {...otherProps}
+            className={cn(styles.li, { [styles.active]: tab === currentTab })}
+            key={tab}>
+            {tab}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
