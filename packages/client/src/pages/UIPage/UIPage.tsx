a229ea3148d76b776c81598137f5639a407321d3
Sviatoslav <lughin.sviatoslav@mail.ru>
2023-05-28 16:57:52 +0300

    PRA-28 refactoring

--- packages/client/src/pages/UIPage/UIPage.tsx
+++ packages/client/src/pages/UIPage/UIPage.tsx
@@ -1,41 +1,48 @@
 import { Button } from 'components/Button';
 import { Input } from '../../components';
 import { FunctionComponent } from 'react';
+import styles from './styles.module.scss';
 
 const UIPage = () => {
   return (
     <>
       <h1>UI Page</h1>
 
-      <h3>Error input 1</h3>
+      <h2>Inputs</h2>
+      <div className={styles.block}>
+        <div>
+          <h3>Error input 1</h3>
+          <Input
+            title="Input 1"
+            inlineTitle={false}
+            deleteSymbol=" × "
+            errorMessage="Error"
+          />
+        </div>
+        <div>
+          <h3>Valid input 1</h3>
+          <Input title="Input 1" inlineTitle={false} deleteSymbol=" × " />
+        </div>
+        <div>
+          <h3>Valid input 2</h3>
+          <Input title="Input 2" inlineTitle />
+        </div>
+        <div>
+          <h3>Error input 2</h3>
+          <Input title="Input 2" inlineTitle errorMessage="Error" />
+        </div>
+      </div>
 
-      <Input
-        title="Input 1"
-        isLabelStatic={false}
-        deleteSymbol=" × "
-        errorMessage="Error"></Input>
-
-      <h3>Valid input 1</h3>
-
-      <Input title="Input 1" isLabelStatic={false} deleteSymbol=" × "></Input>
-
-      <h3>Valid input 2</h3>
-
-      <Input title="Input 2" isLabelStatic={true}></Input>
-
-      <h3>Error input 2</h3>
-
-      <Input title="Input 2" isLabelStatic={true} errorMessage="Error"></Input>
-
-      <h3>Buttons</h3>
-
-      <Button>Default Button</Button>
-      <Button thema="light">Light Button</Button>
-      <Button disabled>Disabled Button</Button>
-      <Button width={100} height={100}>
-        Custom Size
-      </Button>
-      <Button loading>Loading Button</Button>
+      <h2>Buttons</h2>
+      <div className={styles.block}>
+        <Button>Default Button</Button>
+        <Button thema="light">Light Button</Button>
+        <Button disabled>Disabled Button</Button>
+        <Button width={100} height={100}>
+          Custom Size
+        </Button>
+        <Button loading>Loading Button</Button>
+      </div>
     </>
   );
 };
