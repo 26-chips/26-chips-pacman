a06643a34eb8b3ea0cc3f13e905e73be6d4ec826
gitnbrsp <no@e-mail.com>
2023-05-31 01:34:02 +0300

    fix checkbox

--- packages/client/src/components/Checkbox/Checkbox.tsx
+++ packages/client/src/components/Checkbox/Checkbox.tsx
@@ -2,17 +2,10 @@ import { InputHTMLAttributes } from 'react';
 import styles from './checkbox.module.scss';
 
 export const Checkbox = (props: InputHTMLAttributes<HTMLInputElement>) => {
-  const { checked, ...otherProps } = props;
-
   return (
     <div className={styles.container}>
       <label className={styles.checkbox}>
-        <input
-          {...otherProps}
-          defaultChecked={checked}
-          type="checkbox"
-          className={styles.input}
-        />
+        <input {...props} type="checkbox" className={styles.input} />
         <span className={styles.checkmark} />
       </label>
     </div>
