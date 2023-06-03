c6a8f78fb6c45e9767c0aee04a299668c9c79f11
gitnbrsp <no@e-mail.com>
2023-06-03 14:28:34 +0300

    refactor EndGameComponent

--- packages/client/src/components/EndGameScreen/EndGameScreen.tsx
+++ packages/client/src/components/EndGameScreen/EndGameScreen.tsx
@@ -1,44 +1,11 @@
-import styles from './endgamescreen.module.scss';
-import { Button } from '../Button';
 import { Modal, ModalProps } from '../Modal';
-import { Link } from 'react-router-dom';
-import { ROUTES } from 'router';
-import cn from 'classnames';
+import { EndGameComponent } from './EndGameComponent';
 
 export interface EndGameComponentProps {
   username: string;
   score: string;
   className?: string;
   elapsedTimeSec: number | string;
-}
-
-function EndGameComponent({
-  username,
-  score,
-  className,
-  elapsedTimeSec,
-}: EndGameComponentProps): JSX.Element {
-  return (
-    <div className={cn(styles.container, className)}>
-      <h1>{username}</h1>
-      <h1>Ваши результаты</h1>
-      <h1 className={styles.blue}>{score}</h1>
-      <h3 className={styles.blue}>
-        {new Date(Number(elapsedTimeSec) * 1000)
-          .toISOString()
-          .substring(11, 19)}
-      </h3>
-      <div className={styles.buttonContainer}>
-        <Link to={ROUTES.GAME}>
-          <Button>Начать сначала</Button>
-        </Link>
-
-        <Link to={ROUTES.MAIN}>
-          <Button thema="transparent">На главную</Button>
-        </Link>
-      </div>
-    </div>
-  );
 }
 
 export const EndGameScreen = ({
@@ -48,7 +15,7 @@ export const EndGameScreen = ({
   show,
   onClose,
   ...otherProps
-}: EndGameComponentProps & ModalProps) => {
+}: EndGameComponentProps & Omit<ModalProps, 'children'>) => {
   return (
     <Modal {...otherProps} onClose={onClose} show={show}>
       <EndGameComponent
