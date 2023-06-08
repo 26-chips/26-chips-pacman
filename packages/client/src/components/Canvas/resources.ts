763d5c0ee0d68aff816b65dbe344e13267abbcae
Fedorov Vladislav <futuringer.learning@gmail.com>
2023-06-08 13:26:14 +0300

    fix: remove console.logs

--- packages/client/src/components/Canvas/resources.ts
+++ packages/client/src/components/Canvas/resources.ts
@@ -1,6 +1,5 @@
 export function loadImage(url: string) {
   return new Promise(fulfill => {
-    console.log(url);
     const imageObj = new Image();
     imageObj.onload = () => fulfill(imageObj);
     imageObj.src = url;
