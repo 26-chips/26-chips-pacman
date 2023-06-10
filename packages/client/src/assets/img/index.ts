40bc475da3287ec29b6993bd1f92d0ab9857ec9f
gitnbrsp <126918257+gitnbrsp@users.noreply.github.com>
2023-06-10 14:20:37 +0300

    Feature/PRA-13 Error page (#32)
    * resolve
    
    * restore default-avatar.png
    
    * fix errorPage.module.scss
    
    ---------
    
    Co-authored-by: gitnbrsp <no@e-mail.com>

---
+++ packages/client/src/assets/img/index.ts
@@ -0,0 +1,11 @@
+import Green from './Green.svg';
+import Blue from './Blue.svg';
+import Purple from './Purple.svg';
+import Mainpage from './Mainpage.svg';
+
+export const Images: Record<string, string> = {
+  Green,
+  Blue,
+  Purple,
+  Mainpage,
+};
