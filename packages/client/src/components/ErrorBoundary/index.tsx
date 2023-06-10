c7022fa2743db83b9df70c77968c32f802bb9d16
gitnbrsp <no@e-mail.com>
2023-06-11 00:09:32 +0300

    init

---
+++ packages/client/src/components/ErrorBoundary/index.tsx
@@ -0,0 +1,56 @@
+import { ErrorPage } from 'pages';
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
+
+export const ErrorBoundary = () => {
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    return <ErrorPage />;
+  } else {
+    // @ts-ignore
+    return (
+      <ErrorPage
+        statusCode="500"
+        errorText="Внутренняя ошибка сервера :("
+        image="Blue"
+      />
+    );
+  }
+};
+
+// type ParentProps = {
+//     children: ReactNode;
+// };
+//
+// type State = {
+//     error: boolean;
+//     errorInfo: string;
+// };
+//
+// export class ErrorBoundary extends Component {
+//     constructor(props: ParentProps) {
+//         super(props);
+//         this.state = { error: false, errorInfo: '' };
+//     }
+//
+//     componentDidCatch(error: Error) {
+//         console.error(error)
+//
+//         this.setState({
+//             error: Boolean(error),
+//             errorInfo: error.message
+//         })
+//     }
+//
+//     render() {
+//         if (this.state.errorInfo) {
+//             return
+//             <ErrorPage
+//                 statusCode = '500'
+//                 errorText = 'Внутренняя ошибка сервера :(\nМы о ней знаем и скоро все исправим!'
+//                 image = 'Blue'
+//             />;
+//         }
+//         return this.props.children;
+//     }
+// }
