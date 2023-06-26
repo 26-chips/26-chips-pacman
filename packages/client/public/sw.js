f94ed5e30b9e10cebd4f0a1d7bba522ed21c54fb
Sviatoslav <lughin.sviatoslav@mail.ru>
2023-06-26 14:21:55 +0300

    fix cache is not defined

--- packages/client/public/sw.js
+++ packages/client/public/sw.js
@@ -7,14 +7,16 @@ self.addEventListener('message', async ({ data }) => {
 })
 
 self.addEventListener('activate', async () => {
-  console.log('[SW]: activate');
-  const cacheNames = await cache.keys();
+  console.log('[SW]: activate', cache);
+  if (cache) {
+    const cacheNames = await cache.keys();
 
-  await Promise.all(
-    cacheNames
-    .filter(name => name !== CACHE_NAME || name !== D_CACHE_NAME)
-    .map(name => caches.delete(name))
-  );
+    await Promise.all(
+      cacheNames
+      .filter(name => name !== CACHE_NAME || name !== D_CACHE_NAME)
+      .map(name => caches.delete(name))
+    );
+  }
 })
 
 async function cacheFirst(request) {
